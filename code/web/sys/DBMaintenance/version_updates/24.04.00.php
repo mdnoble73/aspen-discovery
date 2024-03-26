844097bf4fffe358e9a14df94bdfa05225c88d8a
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-03-26 16:18:57 -0300

    file_uploads,image_uploads and themes tables have been modified adding columns for data

--- code/web/sys/DBMaintenance/version_updates/24.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.04.00.php
@@ -19,6 +19,33 @@ function getUpdates24_04_00(): array {
 		//kodi - ByWater
 
 		//lucas - Theke
+			'store_files_into_database' => [
+				'title' => 'Store files into database',
+				'description' => 'New column for uploaded files as blobs',
+				'continueOnError' => false,
+				'sql' => [
+					"ALTER TABLE file_uploads ADD COLUMN IF NOT EXISTS uploadedFileData LONGBLOB DEFAULT NULL",
+				]
+			],
+
+			'store_image_into_database' => [
+				'title' => 'Store images into database',
+				'description' => 'New column for uploaded images as blobs',
+				'continueOnError' => false,
+				'sql' => [
+					"ALTER TABLE image_uploads ADD COLUMN IF NOT EXISTS fullSizeImageData LONGBLOB DEFAULT NULL",
+				]
+			],
+
+			'store_custom_fonts_into_database' => [
+				'title' => 'Store custom fonts into database',
+				'description' => 'New columns for uploaded custom fonts as blobs',
+				'continueOnError' => false,
+				'sql' => [
+					"ALTER TABLE themes ADD COLUMN IF NOT EXISTS customHeadingFontData LONGBLOB DEFAULT NULL",
+					"ALTER TABLE themes ADD COLUMN IF NOT EXISTS customBodyFontData LONGBLOB DEFAULT NULL",
+				]
+			]
 
 		//alexander - PTFS Europe
 
